6fa4497df0ec73732fd6b69f272ab4a969cc42ef
Tanmay Gupta <gupta.tanmay.tg@gmail.com>
2025-01-16 00:42:03 +0530

    Updated CSS to tweak minor UI elements

--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Video.css';
+import './video.css';
 import PlayVideo from '../../Components/PlayVideo/PlayVideo';
 import Recommended from '../../Components/Recommended/Recommended';
 import { useParams } from 'react-router-dom';
